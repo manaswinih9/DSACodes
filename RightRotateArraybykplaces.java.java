bd856462db30e546d7f80492ca044e39db48ac6b
manaswinih9 <144710848+manaswinih9@users.noreply.github.com>
2024-11-20 01:08:35 +0530

    Create RightRotateArraybykplaces.java.java

---
+++ RightRotateArraybykplaces.java.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void reverse(int[] arr,int start,int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        k=k%n;
+        if(n==0){
+            return;
+        }
+        reverse(nums,0,n-1-k);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
+        
+    }
+}
