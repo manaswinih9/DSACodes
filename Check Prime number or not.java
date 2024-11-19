e4ae204f54613272615662cd5721f35c686c4012
Manaswini Sharma <144710848+manaswinih9@users.noreply.github.com>
2024-11-20 02:19:02 +0530

    Update Check Prime number or not.java

--- Check Prime number or not.java
+++ Check Prime number or not.java
@@ -1,17 +1,33 @@
-class Solution {
-    static boolean isPrime(int n) {
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    static boolean CheckPrime(int n){
         int cnt=0;
         if(n<2){
             return false;
         }
-        for(int i=2;i*i<=n;i++){
-            if(n%i==0){
-                cnt++;
+        
+         for(int i=2;i*i<=n;i++){
+                if(n%i==0){
+                    cnt++;
+                }
             }
-        }
-        if(cnt==0){
-            return true;
-        }
+            if(cnt==0){
+                return true;
+            }
+        
         return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        boolean isPrime=CheckPrime(num);
+        if(isPrime){
+            System.out.println("prime");
+        }
+        else{
+            System.out.println("not prime");
+        }
     }
 }
